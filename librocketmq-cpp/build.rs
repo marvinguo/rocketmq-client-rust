f3470b049b862e2807d14e21b755f74e2d960217
guoyong <overearth@gmail.com>
2019-02-11 17:08:54 +0800

    先补起文件

--- librocketmq-cpp/build.rs
+++ librocketmq-cpp/build.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+
+fn fail_on_empty_directory(name: &str) {
+    if fs::read_dir(name).unwrap().count() == 0 {
+        println!(
+            "The `{}` directory is empty, did you forget to pull the submodules?",
+            name
+        );
+        println!("Try `git submodule update --init --recursive`");
+        panic!();
     }
+}
+
+fn main() {
+    fail_on_empty_directory("rocketmq-client-cpp");
 }
