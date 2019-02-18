92c662eabda5c6d39d73a34e2ef72e9bf7cccc6a
guoyong <overearth@gmail.com>
2019-02-18 11:39:37 +0800

    封装调用的c函数

--- librocketmq-cpp/src/core.rs
+++ librocketmq-cpp/src/core.rs
@@ -1,9 +1,31 @@
 use binding::*;
 
 pub struct Consumer{
-
+    consumer: * const CPullConsumer,
 }
 
 impl Consumer{
+    fn new(){
 
+    }
+}
+
+pub struct Producer{
+    producer: * const CProducer,
+}
+
+impl Producer{
+    fn new(){
+
+    }
+}
+
+pub struct Message{
+    message: * const CMessage,
+}
+
+impl Message{
+    fn new(){
+
+    }
 }
