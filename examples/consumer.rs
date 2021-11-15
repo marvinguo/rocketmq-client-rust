fa500133458e187284e3d7c763e29cfd3395ff06
marvinguo <overearth@gmail.com>
2021-11-15 23:35:26 +0800

    1

--- examples/consumer.rs
+++
@@ -1,9 +0,0 @@
-use rocketmq_client_rust::core::*;
-use std::{thread, time};
-
-fn main() {
-    let consumer = PushConsumer::new("consumer_test_group", "consumer1", "172.16.208.204:9876");
-    consumer.subscribe("test", "*");
-    consumer.start();
-    thread::sleep(time::Duration::from_secs(100000000));
-}
